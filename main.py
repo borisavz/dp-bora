4afc8cd8e600543e6216598514fd630ba9943c2d
borisavz <borisavzivanovic@gmail.com>
2025-02-08 14:57:54 +0100

    Generate search space for commutativity transformation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from collections import deque
-from typing import List
-
+from typing import List, Set, Iterable
 
 commutative = [True, True, False, False, False, True, False]
 
@@ -84,14 +83,18 @@ class BinaryOperator(Node):
     def __init__(
         self,
         operator_id: int,
-        left: List[Node],
-        right: List[Node],
+        left: Set[Node],
+        right: Set[Node],
         predicate_rejects_null: bool = True
     ):
         self.operator_id = operator_id
         self.left = left
         self.right = right
         self.predicate_rejects_null = predicate_rejects_null
+
+        self.top_parents = set()
+        self.left_parents = set()
+        self.right_parents = set()
 
         self.left_rel_set = set()
         self.right_rel_set = set()
@@ -100,26 +103,54 @@ class BinaryOperator(Node):
             if isinstance(l, Relation):
                 self.left_rel_set.add(l)
             elif isinstance(l, BinaryOperator):
+                l.left_parents.add(self)
+
                 self.left_rel_set.update(l.left_rel_set)
                 self.left_rel_set.update(l.right_rel_set)
-            else:
-                raise Exception()
 
         for r in right:
             if isinstance(r, Relation):
                 self.right_rel_set.add(r)
             elif isinstance(r, BinaryOperator):
+                r.right_parents.add(self)
+
                 self.right_rel_set.update(r.left_rel_set)
                 self.right_rel_set.update(r.right_rel_set)
-            else:
-                raise Exception()
+
+    def add_left_child(self, node):
+        self.left.add(node)
+
+        if isinstance(node, BinaryOperator):
+            node.left_parents.add(self)
+
+    def add_right_child(self, node):
+        self.right.add(node)
+
+        if isinstance(node, BinaryOperator):
+            node.right_parents.add(self)
 
 
-def generate_search_space(query: BinaryOperator):
-    worklist = generate_initial_worklist(query)
+class Result(Node):
+    def __init__(self, children: Set[Node]):
+        self.children = children
+
+        for c in children:
+            if isinstance(c, BinaryOperator):
+                c.top_parents.add(self)
+
+    def add_child(self, node: Node):
+        self.children.add(node)
+
+        if isinstance(node, BinaryOperator):
+            node.top_parents.add(self)
+
+
+def generate_search_space(query: Result):
+    worklist = generate_initial_worklist(query.children)
+    equivalent_nodes = generate_initial_equivalent_nodes(worklist)
 
     while True:
-        new_nodes = iterate_worklist(worklist)
+        new_nodes = iterate_worklist(worklist, equivalent_nodes)
 
         if new_nodes:
             worklist = new_nodes
@@ -127,13 +158,16 @@ def generate_search_space(query: BinaryOperator):
             break
 
 
-def generate_initial_worklist(query: BinaryOperator):
+def generate_initial_worklist(query: Iterable[Node]):
     result = []
 
-    queue = deque([query])
+    queue = deque(query)
 
     while queue:
         node = queue.popleft()
+
+        if not isinstance(node, BinaryOperator):
+            continue
 
         result.append(node)
 
@@ -149,25 +183,100 @@ def generate_initial_worklist(query: BinaryOperator):
     return result
 
 
-def iterate_worklist(worklist: List[BinaryOperator]):
+def generate_initial_equivalent_nodes(nodes: List[BinaryOperator]):
+    equivalent_nodes = dict()
+
+    for n in nodes:
+        key = (
+            n.operator_id,
+            frozenset(n.left_rel_set),
+            frozenset(n.right_rel_set)
+        )
+
+        equivalent_nodes[key] = n
+
+    return equivalent_nodes
+
+
+def equivalent_node_exists(equivalent_nodes, operator_id, left_rel_set, right_rel_set):
+    key = (
+        operator_id,
+        frozenset(left_rel_set),
+        frozenset(right_rel_set)
+    )
+
+    return key in equivalent_nodes
+
+
+def iterate_worklist(worklist: List[BinaryOperator], equivalent_nodes):
     new_nodes = []
 
-    for w in worklist:
+    attempt_transformations = [
+        attempt_commutativity
+    ]
 
-        pass
+    for w in worklist:
+        for attempt_func in attempt_transformations:
+            trans_new_nodes = attempt_func(w, equivalent_nodes)
+            new_nodes.extend(trans_new_nodes)
+
+    return new_nodes
+
+
+def attempt_commutativity(node: BinaryOperator, equivalent_nodes):
+    new_nodes = []
+
+    if (
+        is_commutative(node)
+        and not equivalent_node_exists(
+            equivalent_nodes,
+            node.operator_id,
+            node.right_rel_set,
+            node.left_rel_set
+        )
+    ):
+
+        new_node = BinaryOperator(
+            operator_id=node.operator_id,
+            left=node.right,
+            right=node.left
+        )
+
+        for t in node.top_parents:
+            t.add_child(new_node)
+
+        for l in node.left_parents:
+            l.add_left_child(new_node)
+
+        for r in node.right_parents:
+            r.add_right_child(new_node)
+
+        new_nodes.append(new_node)
 
     return new_nodes
 
 
 if __name__ == '__main__':
-    query = BinaryOperator(
-        operator_id=0,
-        left=[BinaryOperator(
-            operator_id=0,
-            left=[Relation("a")],
-            right=[Relation("b")]
-        )],
-        right=[Relation("c")]
+    query = Result(
+        children={
+            BinaryOperator(
+                operator_id=0,
+                left={
+                    BinaryOperator(
+                        operator_id=0,
+                        left={Relation("a")},
+                        right={
+                            BinaryOperator(
+                                operator_id=0,
+                                left={Relation("c")},
+                                right={Relation("d")}
+                            )
+                        }
+                    )
+                },
+                right={Relation("c")}
+            )
+        }
     )
 
     generate_search_space(query)
